efed6558be11aa7e4a303cd9dad85c83d41a06f3
AyomideKayode <ayomidekay7@gmail.com>
2024-05-09 01:32:37 +0100

    UTF-8 Validation. Code formatting to counter checker edge cases...🚧

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -46,12 +46,11 @@ def validUTF8(data):
                 num_bytes = 2
             elif (byte >> 3) == 0b11110:
                 num_bytes = 3
-            elif byte >> 7:
+            elif (byte >> 7):
                 return False
         else:  # we check if it is not a continuation byte
-            if byte >> 6 != 0b10:
+            if (byte >> 6) != 0b10:
                 return False
             num_bytes -= 1
-
     # if there are trailing bytes left in the data set, it's invalid
     return num_bytes == 0
